60a125f82e42e47e8ef4a0625b0bd5b6fea7a915
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-11 22:57:08 -0500

    Enhanced type safety and configuration flexibility
    The code changes have improved the type safety in the configuration by specifying types for required configurations. A new function, getZod, has been introduced to parse optional configurations. This provides more flexibility as it allows certain configurations to be optional. Several new imports have also been added to accommodate these changes.

--- packages/hosts/config.ts
+++ packages/hosts/config.ts
@@ -1,15 +1,23 @@
 import { Config } from '@pulumi/pulumi';
-import { Bond, Ethernets, Vlan } from 'components';
+import { Arch, Bond, Ethernets, Hosts, Versions, Vlan } from 'components';
 import { z } from 'zod';
 
 const config = new Config();
 
-const requireZod = <T>(parser: z.ZodType<T>, key: string): T => {
-  return parser.parse(config.requireObject(key));
+const getZod = <T>(parser: z.ZodType<T>, key: string): T | undefined => {
+	return parser.optional().parse(config.getObject<T>(key));
 }
 
-export const arch = config.require('arch');
-export const bond = requireZod(Bond, 'bond');
-export const ethernets = requireZod(Ethernets, 'ethernets');
-export const hostname = config.require('hostname');
+const requireZod = <T>(parser: z.ZodType<T>, key: string): T => {
+  return parser.parse(config.requireObject<T>(key));
+}
+
+export const arch = config.require<Arch>('arch');
+export const bond = getZod(Bond, 'bond');
+export const clusterIp = config.require('clusterIp');
+export const ethernets = getZod(Ethernets, 'ethernets');
+export const hostname = config.require<keyof Hosts>('hostname');
+export const installDisk = config.require('installDisk');
+export const ip = config.require('ip');
+export const versions = requireZod(Versions, 'versions');
 export const vlan = requireZod(Vlan, 'vlan');
