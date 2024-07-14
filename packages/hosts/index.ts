d77712579c7b3e5c94cfd40845fd6bc08cbf1520
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-14 01:25:56 -0500

    The last bits I think?

--- packages/hosts/index.ts
+++ packages/hosts/index.ts
@@ -1,6 +1,6 @@
 import { remote } from '@pulumi/command';
 import * as pulumi from '@pulumi/pulumi';
-import { Kubeadm, Kubectl, Netplan, Runner } from 'components';
+import { CniPlugins, Containerd, Crictl, Ipv4PacketForwarding, Kubeadm, Kubectl, Kubelet, Netplan, Runc, Runner } from 'components';
 import { Network } from 'components/src/netplan';
 import * as config from './config';
 
@@ -26,13 +26,13 @@ if (networks.bonds) {
 		source: new pulumi.asset.StringAsset('bonding'),
 	});
 
-	modprobe = runner.run(remote.Command, 'modprobe', {
+	modprobe = runner.run(remote.Command, 'bonding-modprobe', {
 		create: 'modprobe bonding',
 		delete: 'modprobe -r bonding',
 	}, { dependsOn: bondingMod });
 }
 
-const netplan = runner.run(Netplan, 'vlan', {
+const netplan = runner.run(Netplan, name, {
 	config: networks,
 	name: 'thecluster',
 	priority: 69,
@@ -47,3 +47,49 @@ const kubeadm = runner.run(Kubeadm, name, {
 	arch: config.arch,
 	version: config.versions.k8s,
 });
+
+if (config.role === 'worker') {
+	runner.run(Ipv4PacketForwarding, name, {});
+
+	runner.run(CniPlugins, name, {
+		arch: config.arch,
+		version: config.versions.cniPlugins,
+	});
+
+	runner.run(Containerd, name, {
+		arch: config.arch,
+		version: config.versions.containerd,
+	});
+
+	runner.run(Crictl, name, {
+		arch: config.arch,
+		version: config.versions.crictl,
+	});
+
+	runner.run(Kubelet, name, {
+		arch: config.arch,
+		version: config.versions.k8s,
+	});
+
+	runner.run(Runc, name, {
+		arch: config.arch,
+		version: config.versions.runc,
+	});
+}
+
+// // TODO: Not all of these are needed on both localhost and clusterIp
+// // https://kubernetes.io/docs/reference/networking/ports-and-protocols/#control-plane
+// ['127.0.0.1', this.config.clusterIp].map(ip => {
+// 	this.allowPort(ip, 6443); // Kubernetes API server
+// 	this.allowPort(ip, 2379); // etcd server client API
+// 	this.allowPort(ip, 2380); // etcd server client API
+// 	this.allowPort(ip, 10250); // Kubelet API
+// 	this.allowPort(ip, 10259); // kube-scheduler
+// 	this.allowPort(ip, 10257); // kube-controller-manager
+// });
+
+// // https://kubernetes.io/docs/reference/networking/ports-and-protocols/#node
+// ['127.0.0.1', this.config.clusterIp].map(ip => {
+// 	this.allowPort(ip, 10250); // Kubelet API
+// 	this.allowPort(ip, 10256); // kube-proxy
+// });
