1f85050d9d7d63b3963d44f197b1d30c9fb5ed42
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-07-25 19:29:19 -0500

    Add flags for restricting nodes

--- deploy/cmd/root.go
+++ deploy/cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -11,25 +12,41 @@ var (
 	log           = hosts.NewLogger()
 	controlplanes = []string{"pik8s4", "pik8s5", "pik8s6"}
 	workers       = []string{"zeus", "gaea", "vrk8s1", "pik8s8"}
+	cp, wk        bool
 )
 
 var rootCmd = &cobra.Command{
-	Use:       "deploy",
+	Use:       "deploy [OP]",
 	Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	ValidArgs: []string{"down", "preview", "up"},
+	Example:   "deploy up --cp",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := cmd.Context()
-		opt := hosts.Op(args[0])
-
-		return hosts.Deploy(ctx, opt, &hosts.DeployOpts{
+		op := hosts.Op(args[0])
+		opts := hosts.DeployOpts{
 			ControlPlanes: controlplanes,
 			Workers:       workers,
 			Logger:        log,
-		})
+		}
+
+		if cp {
+			fmt.Println("Restricting to controlplanes")
+			opts.Workers = []string{}
+		}
+
+		if wk {
+			fmt.Println("Restricting to workers")
+			opts.ControlPlanes = []string{}
+		}
+
+		return hosts.Deploy(ctx, op, &opts)
 	},
 }
 
 func Execute() {
+	rootCmd.Flags().BoolVar(&cp, "cp", false, "Only operate on controlplanes")
+	rootCmd.Flags().BoolVar(&wk, "wk", false, "Only operate on workers")
+
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
